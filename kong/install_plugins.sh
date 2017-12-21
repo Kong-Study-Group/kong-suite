edff072ce6d326674590780b741dbd9ce0eb7dcc
xuyz <xuyz1988@163.com>
2017-12-21 16:44:59 +0800

    feat add custom plugin kong-plugin-param-transformer

--- kong/install_plugins.sh
+++ kong/install_plugins.sh
@@ -5,7 +5,8 @@ luarocks install kong-plugin-pipeline
 luarocks install kong-plugin-key-secret
 luarocks install kong-plugin-extend-headers
 luarocks install kong-plugin-debug
+luarocks install kong-plugin-param-transformer
 
-sed -i '/#custom_plugins =/i\custom_plugins = key-secret, pipeline, token-agent, extend-headers, debug' /etc/kong/kong.conf
+sed -i '/#custom_plugins =/i\custom_plugins = key-secret, pipeline, token-agent, extend-headers, debug, param-transformer' /etc/kong/kong.conf
 
 exit 0
